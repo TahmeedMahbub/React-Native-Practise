6e8853305c71ee1e9c83a1aeff18976a516a7d40
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2024-01-20 00:01:43 +0600

    props

--- App.tsx
+++ App.tsx
@@ -9,17 +9,22 @@ import React, { useState } from 'react';
 import {Button, Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
-
-  const [name, setName] = useState("Rafid");
-
-  function changeName() {
-    setName("AIUB");
-  }
-
+  var [name, setName] = useState("Rafid");
   return (
     <View>
-      <Text style={{ fontSize:30 }}>{name}</Text>
-      <Button title="Update Name" onPress={changeName}/>
+      <Text style={{ fontSize:30, padding: 5, backgroundColor: "aqua" }}>Parent Component</Text>
+      <Button title='Update Props' onPress={()=>setName("AIUB")} />
+      <ChildComponent name={name} age={25}/>
+    </View>
+  );
+}
+
+const ChildComponent = (props) => {
+  console.warn(props); // PROPS LOOKS LIKE JSON
+  return (
+    <View style={{ padding: 5, backgroundColor: "silver" }}>
+      <Text style={{ fontSize:30 }}>name: {props.name}</Text>
+      <Text style={{ fontSize:30 }}>age: {props.age}</Text>
     </View>
   );
 }
