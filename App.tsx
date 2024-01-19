8a0413dac43e4fe4213ecff63dd319a23e033a04
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2024-01-20 02:47:47 +0600

    FlatList, render individually

--- App.tsx
+++ App.tsx
@@ -6,67 +6,40 @@
  */
 
 import React, { useState } from 'react';
-import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
 import ExtStyles from './style'
 
 function App(): React.JSX.Element {
-  var [name, setName] = useState("");
-  var [email, setEmail] = useState("");
-  var [password, setPassword] = useState("");
-  var [showPassword, setShowPassword] = useState(true);
-  var [display, setDisplay] = useState(false);
+  var users = [
+    {
+      id: 1,
+      name: "A"
+    },
+    {
+      id: 2,
+      name: "B"
+    },
+    {
+      id: 3,
+      name: "C"
+    },
+    {
+      id: 4,
+      name: "D"
+    }
+  ];
 
-  var resetFormData=()=> {
-    setName("");
-    setEmail("");
-    setPassword("");
-    setDisplay(false);
-  }
   return (
     <View>
-      <Text style={{ fontSize:30, padding: 10, backgroundColor: "#737373", color: "white" }}>Simple Form </Text>
+      <Text style={{ fontSize:30, padding: 10, backgroundColor: "#737373", color: "white" }}>Flat List Component</Text>
 
-      <Text style={{ fontSize:20, marginLeft: 10, marginTop: 10 }}>Your Name: {name} </Text>
-      <TextInput 
-        style={style.textInput}
-        placeholder='Enter Your Name'
-        value={name}
-        onChangeText={(text)=>setName(text)}
+      <FlatList 
+        data={users}
+        renderItem={({item})=><Text style={ExtStyles.textBox}>{item.id}. {item.name}</Text>}
+        keyExtractor={item=>item.id}
       />
 
-      <TextInput 
-        style={style.textInput}
-        placeholder='Enter Your Email'
-        value={email}
-        onChangeText={(text)=>setEmail(text)}
-      />
 
-      <TextInput 
-        style={style.textInput}
-        placeholder='Enter Your Password'
-        value={password}
-        secureTextEntry={showPassword}
-        onChangeText={(text)=>setPassword(text)}
-      />
-
-      <Button title="Show Password" onPress={()=>setShowPassword(showPassword ? false : true)}/>
-      <View style={{ marginBottom: 10, marginTop: 10 }}>
-        <Button color={"#3CB371"} title="Print Values" onPress={()=>setDisplay(true)}/>
-      </View>
-      <Button color={"#FA8072"} title="Clear Values" onPress={resetFormData}/>
-
-      <View>
-        {
-          display ? 
-            <View>
-              <Text style={{ fontSize: 30, fontWeight: 1000, margin: 10 }}>Your Data: </Text> 
-              <Text style={ExtStyles.textBox}>Your Name: {name}</Text> 
-              <Text style={ExtStyles.textBox}>Your Email: {email}</Text> 
-              <Text style={ExtStyles.textBox}>Your Password: {password}</Text> 
-            </View>
-            : null
-        }
-      </View>
     </View>
   );
 }
