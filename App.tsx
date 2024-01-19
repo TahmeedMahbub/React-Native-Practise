dce39fdab80540c8f28839b18d0733a082daea0e
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2024-01-20 01:31:43 +0600

    4 types of css (inline, internal, external, multiple classes)

--- App.tsx
+++ App.tsx
@@ -6,28 +6,42 @@
  */
 
 import React, { useState } from 'react';
-import {Button, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import ExtStyles from './style';
 
 function App(): React.JSX.Element {
   var [name, setName] = useState("Rafid");
   return (
     <View>
-      <Text style={{ fontSize:30, padding: 5, backgroundColor: "aqua" }}>Parent Component</Text>
-      <Button title='Update Props' onPress={()=>setName("AIUB")} />
-      <ChildComponent name={name} age={25}/>
+      <Text style={{ fontSize:30, padding: 5, backgroundColor: "black", color: "white", padding: 10, textAlign: 'center', }}>Inline CSS {/* DOUBLE CURLY BRACE. ONE FOR STYLE, ANOTHER FOR JSX */} </Text>
+      
+      <Text style={styles.textBox}>Internal CSS {/* SINGLE CURLY BRACE */} </Text>
+
+      <Text style={ExtStyles.textBox}>External CSS {/* SINGLE CURLY BRACE */} </Text>
+
+      <Text style={[styles.style1, ExtStyles.style2, {padding: 10}]}>Multiple Class CSS {/* ARRAY FOR MULTIPLE CLASSES, ANOTHER CURLY BRACE FOR JSX */} </Text>
     </View>
   );
 }
 
-const ChildComponent = (props) => {
-  console.warn(props); // PROPS LOOKS LIKE JSON
-  return (
-    <View style={{ padding: 5, backgroundColor: "silver" }}>
-      <Text style={{ fontSize:30 }}>name: {props.name}</Text>
-      <Text style={{ fontSize:30 }}>age: {props.age}</Text>
-    </View>
-  );
-}
+const styles = StyleSheet.create({
+  textBox: {
+    fontSize: 25,
+    backgroundColor: 'silver',
+    height: 100,
+    textAlignVertical: 'center',
+    padding: 10,
+    margin: 7,
+    borderRadius: 10
+  },
+
+  style1: {
+    fontSize: 25,
+    backgroundColor: 'aqua',
+    height: 100,
+    textAlignVertical: 'center',
+  }
+})
 
 
 export default App;
---
+++ style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default ExtStyles = StyleSheet.create({ // IMPORTANT! NEED TO USE EXPORT DEFAULT
+    textBox: {
+        fontSize: 25,
+        backgroundColor: 'silver',
+        height: 100,
+        textAlignVertical: 'center',
+        textAlign: 'right',
+        padding: 10,
+        margin: 7,
+        borderRadius: 10
+    },
+    style2 : {
+        margin: 7,
+        borderRadius: 10
+    }
+})
