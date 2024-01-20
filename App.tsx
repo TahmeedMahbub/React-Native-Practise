5586c1ac1a9a2285f7c0ba5cb2dd5703a47cda70
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2024-01-20 22:54:30 +0600

    Custom List By Map Function With ScrollView

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { useState } from 'react';
-import {Button, FlatList, StyleSheet, Text, TextInput, View} from 'react-native';
+import {Button, ScrollView, StyleSheet, Text, TextInput, View} from 'react-native';
 import ExtStyles from './style'
 
 function App(): React.JSX.Element {
@@ -26,20 +26,54 @@ function App(): React.JSX.Element {
     {
       id: 4,
       name: "D"
-    }
+    },
+    {
+      id: 5,
+      name: "E"
+    },
+    {
+      id: 6,
+      name: "F"
+    },
+    {
+      id: 7,
+      name: "G"
+    },
+    {
+      id: 8,
+      name: "H"
+    },
+    {
+      id: 9,
+      name: "I"
+    },
+    {
+      id: 10,
+      name: "J"
+    },
+    {
+      id: 11,
+      name: "K"
+    },
+    {
+      id: 12,
+      name: "L"
+    },
+    {
+      id: 13,
+      name: "M"
+    },
   ];
 
   return (
     <View>
       <Text style={{ fontSize:30, padding: 10, backgroundColor: "#737373", color: "white" }}>Flat List Component</Text>
 
-      <FlatList 
-        data={users}
-        renderItem={({item})=><Text style={ExtStyles.textBox}>{item.id}. {item.name}</Text>}
-        keyExtractor={item=>item.id}
-      />
-
-
+      <ScrollView style={{ marginBottom:60 }}>
+        {
+          users.map((item)=><Text style={ExtStyles.textBox}>({item.id}) {item.name}</Text>)
+        }
+      </ScrollView>
     </View>
   );
 }
