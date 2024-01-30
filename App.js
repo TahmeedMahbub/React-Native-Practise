abf7319337b6002579a266723a0591b79bf0cc96
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2024-01-30 22:10:59 +0600

    important, hook: useEffect

--- App.js
+++ App.js
@@ -1,37 +1,21 @@
-import React, { Component } from 'react';
-import {Button, Text, TextInput, View} from 'react-native';
-import Student from './components/Student';
+import React, {useEffect, useState} from 'react';
+import {View, Text, Button} from 'react-native';
 
-class App extends Component {
-  constructor () { // CONSTRUCTOR IS NEEDED FOR CLASS COMPONENT
-    super(); // SUPER KEYWORD IS USED FOR CALL CONSTRUCTOR OF PARENT CLASS
-    this.state={
-      name: "Rafid",
-      age: "25"
-    }
-  }
+const App = () => {
+  const [count, setCount] = useState(0);
 
-  updateName(val) {
-    this.setState({name:val});
-  }
+  useEffect (()=>{
+    console.warn("Hello")
+  }, [])
 
-  
-  render(){
-    return (
-      <View>
-        <Text style={{ fontSize:30, padding: 10, backgroundColor: "#737373", color: "white" }}>Class Component</Text>
-        <Text style={{ fontSize:20, padding: 10}}>{this.state.name}'s Form</Text>
-        <TextInput 
-          placeholder="Enter Name" 
-          style={{ fontSize:20, padding: 10}} 
-          onChangeText={(text)=>this.updateName(text)}
-        />
-        <Button title="Press Here" />
-        <Student name={this.state.name} />
-      </View>
-    )
-  }
-
+  return (
+    <View>
+      <Text style={{ fontSize:30, padding: 10, backgroundColor: "#737373", color: "white" }}>Life Cycle using useEffect</Text>
+      <Text style={{ fontSize:25, padding: 10}}>{count}</Text>
+      <Button title="Update" onPress={()=>setCount(count+1)} />
+    </View>
+  )
 }
+
 
 export default App;
