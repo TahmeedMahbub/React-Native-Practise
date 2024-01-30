5b43e4b4fc445d5817d13e20adca9f1660b02f2f
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2024-01-31 00:44:18 +0600

    Unmount Using useEffect

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Button} from 'react-native';
 
 const App = () => {
@@ -6,9 +6,7 @@ const App = () => {
 
   return (
     <View>
-      <Text style={{ fontSize:25, padding: 10, backgroundColor: "#737373", color: "white" }}>Show / Hide Component</Text>
-      <Button title="Show Component" onPress={()=>setShow(true)} />
-      <Button title="Hide Component" onPress={()=>setShow(false)} />
+      <Text style={{ fontSize:25, padding: 10, backgroundColor: "#737373", color: "white" }}>useEffect for Unmount</Text>
       <Button title="Toggle Component" onPress={()=>setShow(!show)} />
       {show ? <User /> : null}
     </View>
@@ -16,6 +14,13 @@ const App = () => {
 }
 
 const User=()=>{
+  let timer = setInterval(()=>{
+    console.warn("timer called");
+  }, 2000)
+
+  useEffect(()=>{
+    return()=>clearInterval(timer)
+  })
   return (
     <Text style={{ fontSize:20, padding: 10 }}>User Component </Text>
   )
