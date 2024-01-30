180e5466512036c8a78745344ae3160cd91c3e2c
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2024-01-31 00:17:45 +0600

    unmount component (show/hide/toggle) useState

--- App.js
+++ App.js
@@ -1,41 +1,21 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {View, Text, Button} from 'react-native';
 
 const App = () => {
-  const [count, setCount] = useState(0);
-  const [effect, setEffect] = useState(0);
-  const [data, setData] = useState(0);
-
-  // useEffect (()=>{
-  //   console.warn("Called from useEffect")
-  // }, [count])
-
-  // useEffect (()=>{
-  //   console.warn("Called from another useEffect")
-  // }, [effect])
+  const [show, setShow] = useState(false);
 
   return (
     <View>
-      <Text style={{ fontSize:25, padding: 10, backgroundColor: "#737373", color: "white" }}>useEffect as componentDidUpdate</Text>
-      <Text style={{ fontSize:20, padding: 10}}>Count: {count} </Text>
-      <Text style={{ fontSize:20, padding: 10}}>Effect: {effect} </Text>
-      <Text style={{ fontSize:20, padding: 10}}>Data: {data} (useEffect NOT Called)</Text>
-      <Button title="Update Count" onPress={()=>setCount(count+1)} />
-      <Button title="Update Effect" onPress={()=>setEffect(effect+1)} />
-      <Button title="Update Data" onPress={()=>setData(data+1)} />
-      <User info={{ count, effect, data }}/>
+      <Text style={{ fontSize:25, padding: 10, backgroundColor: "#737373", color: "white" }}>Show / Hide Component</Text>
+      <Button title="Show Component" onPress={()=>setShow(true)} />
+      <Button title="Hide Component" onPress={()=>setShow(false)} />
+      <Button title="Toggle Component" onPress={()=>setShow(!show)} />
+      {show ? <User /> : null}
     </View>
   )
 }
 
-const User=(props)=>{
-  useEffect(()=>{
-    console.warn("data updated")
-  }, [props.info.data]);
-
-  useEffect(()=>{
-    console.warn("count updated")
-  }, [props.info.count]);
+const User=()=>{
   return (
     <Text style={{ fontSize:20, padding: 10 }}>User Component </Text>
   )
