c89a45107e88dd042f7e29ef2449f1f5977b8582
Tahmeed Mahbub <49299194+TahmeedMahbub@users.noreply.github.com>
2024-01-31 23:18:26 +0600

    Radio Button: TouchableOpacity

--- App.js
+++ App.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const Header=()=>{
+  return(
+    <Text style={style.header}>
+      Radio Button - TouchableOpacity
+    </Text>
+    );
+}
+
 
 const App = () => {
+
+const [checkRadio, setCheckRadio] = useState(0);
+
   return (
-    <View>
-      <Text style={{ fontSize:25, padding: 10, backgroundColor: "#737373", color: "white" }}>
-        TouchableHighlight
-      </Text>
-      <TouchableHighlight>
-        <Text style={[style.button, style.success]}>Success</Text>
-      </TouchableHighlight>
-      <TouchableHighlight>
-        <Text style={style.button}>Button</Text>
-      </TouchableHighlight>
+    <View style={style.main}>
+      <Header />
+
+      <View style={style.body}>
+
+        <TouchableOpacity onPress={()=>setCheckRadio(1)}>
+          <View style={style.radioWrapper}>
+            <View style={style.radio}>
+              {checkRadio == 1 ? <View style={style.radioBg} /> : null}
+            </View>
+            <Text style={style.radioText}>Radio 1</Text>
+          </View>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={()=>setCheckRadio(2)}>
+          <View style={style.radioWrapper}>
+            <View style={style.radio}>
+              {checkRadio == 2 ? <View style={style.radioBg} /> : null}
+            </View>
+            <Text style={style.radioText}>Radio 2</Text>
+          </View>
+        </TouchableOpacity>
+
+      </View>
     </View>
     
   )
@@ -22,6 +48,39 @@ const style = StyleSheet.create({
   main: {
     flex: 1
   }, 
+  header: {
+    fontSize:25, 
+    padding: 10, 
+    backgroundColor: "#737373", 
+    color: "white"
+  },
+  body: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  radio:{
+    height: 40, 
+    width: 40,
+    borderColor: 'black',
+    borderWidth: 2,
+    borderRadius: 30,
+    margin: 10,
+  },
+  radioBg: {
+    backgroundColor: 'black',
+    height: 28,
+    width: 28,
+    margin: 4,
+    borderRadius: 30,
+  },
+  radioText:{
+    fontSize: 20,
+  },
+  radioWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
   button : {
     backgroundColor: 'skyblue',
     fontSize: 20,
